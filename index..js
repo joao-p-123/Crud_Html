5f368f944283bd0924800f918739951d89d7567f
joao_pedro <joaopedrovieiradossantos326@gmail.com>
2025-03-10 12:42:14 -0300

    tratamento de erros

--- index..js
+++ index..js
@@ -1,10 +1,7 @@
 function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById('recover-passeord-button').disabled = !emailValid;
-
-    const password = isPasswordValid();
-    document.getElementById('login-button').disabled = !emailValid || !isPasswordValid;
-
+    
+    toggleEmailErrors();
+    toggleButtonDisable();
 
 
 }
@@ -18,8 +15,30 @@ function isEmailValid() {
 
 }
 
+function toggleEmailErrors(){
+    const email = document.getElementById('email').value;
+    if(email){
+        document.getElementById('email-require-error').style.display = "block";
+
+    }else{
+        document.getElementById('email-require-error').style.display = "none";
+
+    }
+    
+
+}
+
+function toggleButtonDisable(){
+    const emailValid = isEmailValid();
+    document.getElementById('recover-passeord-button').disabled = !emailValid;
+
+    const password = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !isPasswordValid;
+
+}
+
 function isPasswordValid() {
-    const password = document.getElementById(password);
+    const password = document.getElementById(password).value;
     if (!password) {
         return false;
     }
